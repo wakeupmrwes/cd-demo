f9f86d119f945f5be275e01120aec2ab1dd7f46e
Ben Lin <ben.lin@mesosphere.com>
2016-10-05 06:52:24 -0700

    update

---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+bin/demo.py install http://54.169.214.197/
+bin/demo.py pipeline --branch=ben-demo-branch --password=zcTzRHzKcnN1Gg http://benlin-sx-publicsl-11f91ind8i9kd-763837412.us-west-2.elb.amazonaws.com/ http://54.169.214.197/
